c679a7259e0eaf997c6a75e6572d89ce86cf2026
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-07-30 16:43:04 -0400

    commit

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ const slideshowDivs = () => {
 
         div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
 
+        i === 1 && div.classList.add('change')
+
         document.querySelector('.slideshow').appendChild(div)
     }
 }
 
 slideshowDivs()
+
+const divs = document.querySelectorAll('slideshow div')
+
+const slideshow = () => {
+    setInterval(() =>{
+        const div = document.querySelector('slideshow .change')
+        div.classList.remove('change')
+        div.nextElementSibling.classList.add('change')
+    }, 1000)
+}
+
+slideshow()
 
 // End of Slideshow
 
