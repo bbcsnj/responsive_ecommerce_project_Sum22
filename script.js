7b58ab9cf70263341f5f12df3736715a58118285
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-07-30 16:16:37 -0400

    commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Slideshow
 
 const slideshowDivs = () => {
-    for(let i = 1; i <= 5; i++){
+    for(let i = 1; i <= 5; i++) {
         const div = document.createElement('div')
 
-        div.style.backgroundImage = 'url(images/slideshow/section-1-bg-${i}.jpg'
+        div.style.backgroundImage = 'url(images/slideshow/section-1-bg-${i}.jpg)'
 
         document.querySelector('.slideshow').appendChild(div)
     }
