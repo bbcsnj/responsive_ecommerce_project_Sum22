fa19adf1647afba47003b1a54a92e02fe7314cf4
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-08-01 18:16:48 -0400

    c

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const playPause = () => {
 
 playPause()
 
-document.querySelector('.controls').
+/*document.querySelector('.controls').
 addEventListener('mouseover', () => {
     bool = false
     playPause()
@@ -53,7 +53,7 @@ document.querySelector('.controls').
 addEventListener('mouseout', () => {
     bool = true
     playPause()
-})
+})*/
 
 // End of Cube
 
