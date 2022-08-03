8d5707c8ad7d6f477cba0a9c4f9ed28fa45f86a5
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-08-03 12:30:07 -0400

    c

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ slideshow()
 // End of Slideshow
 
 // Section 3
-const section3Content = document.querySelector('.section-3-contnet')
+const section3Content = document.querySelector('.section-3-content')
 
 window.addEventListener('scroll', () => {
     if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2){
